1865b52da5f28aeca2bb11202134445eed8805da
realcanadafirst <realcanadafirst@gmail.com>
2024-05-12 17:31:21 +0530

    sequence settngs

--- src/pages/sequences/view/[slug].js
+++ src/pages/sequences/view/[slug].js
@@ -1,4 +1,4 @@
-import { useState, useEffect  } from "react";
+import { useState, useEffect } from "react";
 import DefaultLayout from "@ft/ui-components/admin/Layouts/DefaultLayout";
 import Breadcrumb from '@ft/ui-components/admin/breadcrumb';
 import Alert from "@ft/ui-components/admin/Alert";
@@ -68,7 +68,7 @@ export default function SequenceUpdate() {
             }
         });
     }
-  
+
     const createSequenceApi = () => {
         if (activeTab === 'styled-prospects') {
             assignProspects();
@@ -135,12 +135,37 @@ export default function SequenceUpdate() {
         setFormData(formDataTemp)
     }
 
+    const saveSettings = (data) => {
+        if (data.name && data.name !== '' && data.replies && data.replies !== '' && data.mailbox && data.mailbox !== '' && data.meetings && data.meetings !== '') {
+            const postData = {
+                id: slug,
+                name: data.name,
+                replies: data.replies,
+                mailbox: data.mailbox,
+                meetings: data.meetings,
+            };
+            setMessage({ msg: '', type: '' });
+            fetchData('/api/v1/sequences', 'POST', postData).then((res) => {
+                if (res.status === 'success') {
+                    setMessage({ msg: 'Sequence created successfully!', type: 'success' });
+                    setTimeout(()=>{
+                      router.push({ pathname: `/sequences/view/${slug}`, query: { status: 'success' } });
+                    },1000)
+                } else {
+                    setMessage({ msg: 'Please fill all required fields', type: 'error' });
+                }
+            });
+        } else {
+            setMessage({ msg: 'Please fill all required fields', type: 'error' });
+        }
+    }
+
     return (
         <DefaultLayout>
             {slug ? <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
                 <div className="mx-auto max-w-270">
                     <Breadcrumb pageName={`Sequence: ${sequence?.name}`} />
-                   {message.msg ? <Alert message={message} setMessage={setMessage} /> : null}
+                    {message.msg ? <Alert message={message} setMessage={setMessage} /> : null}
                     <div className="mb-4 border-b border-gray-200 dark:border-gray-700 flex justify-between items-center">
                         <ul className="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-styled-tab" data-tabs-toggle="#default-styled-tab-content" data-tabs-active-classes="text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500" data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300" role="tablist">
                             <li className="me-2" role="presentation">
@@ -176,7 +201,7 @@ export default function SequenceUpdate() {
                         </div>
                         <div className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${activeTab === 'styled-settings' ? 'block' : 'hidden'
                             }`} id="styled-settings" role="tabpanel" aria-labelledby="settings-tab">
-                            <SequenceSetting />
+                            {slug ? <SequenceSetting id={slug} sequence={sequence} saveSettings={saveSettings} /> : null}
                         </div>
                         <div className={`p-4 rounded-lg bg-gray-50 dark:bg-gray-800 ${activeTab === 'styled-prospects' ? 'block' : 'hidden'
                             }`} id="styled-prospects" role="tabpanel" aria-labelledby="prospects-tab">
--- src/ui-components/admin/SequenceSetting.js
+++ src/ui-components/admin/SequenceSetting.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
-import Alert from "@ft/ui-components/admin/Alert";
-const SequenceSetting = () => {
+import { useState, useEffect } from "react";
+const SequenceSetting = ({ id, sequence, saveSettings }) => {
     const [formData, setFormData] = useState({
-        stepType: '',
-        intervalTimeDay: '',
-        intervalTimeHour: '',
-        intervalTimeMin: '',
-        taskPriority: 0,
-        taskNote: '',
-        selectTemplate: '',
-        status: 1
+        name: '',
+        mailbox: '',
+        replies: '',
+        meetings: '',
+        sequenceType: ''
     });
-    const [message, setMessage] = useState({ msg: '', type: '' });
+    useEffect(() => {
+        if (sequence && sequence?.name) {
+            setFormData({
+                name: sequence.name,
+                mailbox: sequence.from_email,
+                replies: sequence.replies,
+                meetings: sequence.meetings,
+                sequenceType: sequence.sequenceType == '0' ? 'Steps by day interval' : ' Steps by exact date/time'
+            })
+        }
+
+    }, [sequence])
     const handleChange = (e) => {
         const formDataTemp = { ...formData }
         formDataTemp[e.target.name] = e.target.value;
@@ -20,31 +27,84 @@ const SequenceSetting = () => {
     return (
         <>
             <div className="p-6.5 min-w-80 min-w-52">
-                <Alert message={message} setMessage={setMessage} />
-                <div className="mb-5.5">
-                    <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="name">Name</label>
-                    <input
-                        className="w-full rounded border border-stroke bg-gray py-3 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                        onChange={handleChange}
-                        value={formData.name}
-                    />
+                <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
+                    <div className="w-full sm:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="name">Name</label>
+                        <div className="relative">
+                            <input
+                                className="w-full rounded border border-stroke py-3 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                                type="text"
+                                name="name"
+                                placeholder="Name"
+                                onChange={handleChange}
+                                value={formData.name}
+                            />
+                        </div>
+                    </div>
+                    <div className="w-full sm:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="mailbox">Mail from</label>
+                        <div className="relative">
+                            <input
+                                className="w-full rounded border border-stroke py-3 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                                type="text"
+                                name="mailbox"
+                                placeholder="Mailbox"
+                                onChange={handleChange}
+                                value={formData.mailbox}
+                            />
+                        </div>
+                    </div>
                 </div>
-                
-                <div className="mb-5.5">
-                    <label className="mb-3 block text-sm font-medium text-black dark:text-white" htmlFor="mailbox">Mail from</label>
-                    <input
-                        className="w-full rounded border border-stroke bg-gray py-3 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-                        type="text"
-                        name="mailbox"
-                        placeholder="Mailbox"
-                        onChange={handleChange}
-                        value={formData.mailbox}
-                    />
+                <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
+                    <div className="w-full sm:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white" > When The Primary Prospect Replies </label>
+                        <div className="relative">
+                            <select
+                                value={formData.replies}
+                                name="replies"
+                                onChange={handleChange}
+                                className={`relative z-20 w-full appearance-none rounded border border-stroke px-12 py-3  pl-2.5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input`}
+                            >
+                                <option value="1" className="text-body dark:text-bodydark">Finish Sequence</option>
+                                <option value="2" className="text-body dark:text-bodydark">Pause Sequence</option>
+                                <option value="3" className="text-body dark:text-bodydark">Continue Sequence</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="w-full sm:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white" >When The Primary Prospect Books Meeting </label>
+                        <div className="relative">
+                            <select
+                                value={formData.meetings}
+                                name="meetings"
+                                onChange={handleChange}
+                                className={`relative z-20 w-full appearance-none rounded border border-stroke px-12 py-3  pl-2.5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input`}
+                            >
+                                <option value="1" className="text-body dark:text-bodydark">Finish Sequence</option>
+                                <option value="2" className="text-body dark:text-bodydark">Pause Sequence</option>
+                                <option value="3" className="text-body dark:text-bodydark">Continue Sequence</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
-
+                <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
+                    <div className="w-full sm:w-1/2">
+                        <label className="mb-3 block text-sm font-medium text-black dark:text-white" >Sequence Type </label>
+                        <div className="relative">
+                            <input
+                                className="w-full rounded border border-stroke py-3 pr-4.5 text-black"
+                                type="text"
+                                readOnly={true}
+                                value={formData.sequenceType}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-end gap-4.5">
+                    <button className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90" onClick={() => { saveSettings(formData) }}>
+                        Save
+                    </button>
+                </div>
             </div>
         </>
     );
