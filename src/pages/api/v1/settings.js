8e9d9749a0c631e0af4f417b16834bcfec6aff82
realcanadafirst <realcanadafirst@gmail.com>
2024-05-24 00:15:42 +0530

    Update settings.js

--- src/pages/api/v1/settings.js
+++ src/pages/api/v1/settings.js
@@ -1,4 +1,5 @@
-import { createConnection } from 'mysql2';
+import { createConnection } from '@ft/lib/dbconnection';
+
 export default function handler(req, res) {
     if (req.method === 'GET') {
         handleGetRequest(req, res);
@@ -11,20 +12,16 @@ export default function handler(req, res) {
 }
 async function handleGetRequest(req, res) {
     try {
-        const connection = createConnection({ host: process.env.RDS_HOSTNAME, user: process.env.RDS_USERNAME, password: process.env.RDS_PASSWORD, database: process.env.RDS_DATABASE });
-        connection.connect((err) => {
-            if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; }
-        });
-        let query = 'SELECT * FROM `settings`';
-        connection.query(query, (err, results) => {
-            if (err) {
-                res.status(500).json({ error: 'Failed to get data from database' });
-                return;
-            }
+        const connection = await createConnection();
+        connection.connect((err) => { if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; } });
+        try {
+            const [results] = await connection.execute('SELECT * FROM `settings`');
             res.status(200).json({ data: results });
-        });
-        connection.end();
-
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to get data from database.' });
+        } finally {
+            await connection.end();
+        }
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -32,27 +29,25 @@ async function handleGetRequest(req, res) {
 
 async function handlePostRequest(req, res) {
     try {
-        const connection = createConnection({ host: process.env.RDS_HOSTNAME, user: process.env.RDS_USERNAME, password: process.env.RDS_PASSWORD, database: process.env.RDS_DATABASE });
-        connection.connect((err) => {
-            if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; }
-        });
-
-        const body = req.body;
-        let query = 'UPDATE settings SET value = CASE attribute ';
-        body.forEach(update => {
-            query += `WHEN '${update.attribute}' THEN '${update.value}' `;
-        });
-        query += 'END WHERE attribute IN (';
-        query += body.map(update => `'${update.attribute}'`).join(', ');
-        query += ')';
-
-        connection.query(query, (err, results) => {
-            console.log(err)
-            if (err) { res.status(500).json({ error: 'Failed to insert data' }); return; }
+        const connection = await createConnection();
+        connection.connect((err) => { if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; } });
+        try {
+            const body = req.body;
+            let query = 'UPDATE settings SET value = CASE attribute ';
+            body.forEach(update => {
+                query += `WHEN '${update.attribute}' THEN '${update.value}' `;
+            });
+            query += 'END WHERE attribute IN (';
+            query += body.map(update => `'${update.attribute}'`).join(', ');
+            query += ')';
+            const [results] = await connection.execute(query);
             res.status(200).json({ data: results });
-        });
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to get data from database.' });
+        } finally {
+            await connection.end();
+        }
     } catch (error) {
-        console.log(error)
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
