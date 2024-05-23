390742ae29ce3d1c997d00b5610faf06b85465b9
realcanadafirst <realcanadafirst@gmail.com>
2024-05-23 21:45:37 +0530

    update query

--- src/pages/api/v1/sequence/steps.js
+++ src/pages/api/v1/sequence/steps.js
@@ -1,37 +1,38 @@
-import { createConnection } from 'mysql2';
-const { promisify } = require('util');
+const mysql = require('mysql2/promise');
 
 export default function handler(req, res) {
     if (req.method === 'GET') {
         handleGetRequest(req, res);
     } else if (req.method === 'POST') {
         handlePostRequest(req, res);
-    } else if (req.method === 'PUT') {
-        handlePutRequest(req, res);
-    } else if (req.method === 'DELETE') {
-        handleDeleteRequest(req, res);
     } else {
         res.setHeader('Allow', ['GET', 'POST', 'DELETE', 'PUT']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
-
+const createConnection = async () => {
+    return mysql.createConnection({
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DATABASE
+    });
+};
 async function handleGetRequest(req, res) {
     try {
-        const connection = createConnection({ host: process.env.RDS_HOSTNAME, user: process.env.RDS_USERNAME, password: process.env.RDS_PASSWORD, database: process.env.RDS_DATABASE });
+        const connection = await createConnection();
         connection.connect((err) => { if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; } });
-        const query = promisify(connection.query).bind(connection);
         try {
             const c_id = req.query?.s_id ? req.query?.s_id : null;
-            const steps = await query(`SELECT * FROM sequence_steps WHERE sequence_id = ${c_id}`);
-            res.status(200).json({ data: steps });
+            const query = `SELECT * FROM sequence_steps WHERE sequence_id = ${c_id}`;
+            const [results] = await connection.execute(query);
+            res.status(200).json({ data: results });
             return;
         } catch (error) {
-            console.log(error)
-            res.status(500).json({ error: 'Failed to get data from database.' });
-            return;
+            res.status(500).json({ error: 'Failed to get data BB' });
+        } finally {
+            await connection.end();
         }
-        connection.end(); // Close the connection
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -39,97 +40,39 @@ async function handleGetRequest(req, res) {
 
 async function handlePostRequest(req, res) {
     try {
-        const connection = createConnection({ host: process.env.RDS_HOSTNAME, user: process.env.RDS_USERNAME, password: process.env.RDS_PASSWORD, database: process.env.RDS_DATABASE });
-        connection.connect((err) => {
-            if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; }
-        });
-        const step = req.query?.step ? req.query?.step : null;
-        console.log(req.query)
-        if (step) {
-            if (req.query?.test) {
-                sendTestEmail(req, res);
-            } else {
-                const { subject, template } = req.body;
-                const query = `UPDATE sequence_steps SET subject = ?, template = ? WHERE id = ?`;
-                connection.query(query, [subject, template, step], (err, results) => {
-                    if (err) { res.status(500).json({ error: 'Failed to insert data' }); return; }
-                    res.status(200).json({ data: results });
-                });
-            }
-        } else {
-            const c_id = req.query?.s_id ? req.query?.s_id : null;
-            const { stepType, intervalTime, taskPriority, taskNote, subject, template, status, execution_date } = req.body;
-            let query = 'INSERT INTO sequence_steps (sequence_id, step_type, interval_time, execution_date, priority, note, subject, template, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-            connection.query(query, [c_id, stepType, intervalTime, execution_date, taskPriority, taskNote, subject, template, status], (err, results) => {
-                if (err) {
-                    console.log(err)
-                    res.status(500).json({ error: 'Failed to insert data' });
-                    return;
-                }
-                res.status(200).json({ data: results });
-            });
-        }
-        connection.end();
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-async function handlePutRequest(req, res) {
-    try {
-        const c_id = req.query?.c_id;
-        const status = req.query?.status;
-        if (c_id, status) {
-            const connection = createConnection({ host: process.env.RDS_HOSTNAME, user: process.env.RDS_USERNAME, password: process.env.RDS_PASSWORD, database: process.env.RDS_DATABASE });
-            connection.connect((err) => { if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; } });
-            if (status == 3) {
-                const query = `DELETE FROM sequence_prospects WHERE id = ${c_id}`;
-                connection.query(query, (err, results) => {
-                    if (err) { res.status(500).json({ error: 'Failed to delete data from database' }); return; }
-                    res.status(200).json({ data: results });
-                });
-                connection.end();
-            } else {
-                const query = `UPDATE sequence_prospects SET sequence_status = ? WHERE id = ?`;
-                connection.query(query, [status, c_id], (err, results) => {
-                    if (err) {
-                        res.status(500).json({ error: 'Failed to get data from database' });
-                        return;
+        const connection = await createConnection();
+        connection.connect((err) => { if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; } });
+        try {
+            const step = req.query?.step ? req.query?.step : null;
+            if (step) {
+                if (req.query?.test) {
+                    sendTestEmail(req, res);
+                } else {
+                    try {
+                        const { subject, template } = req.body;
+                        const query = `UPDATE sequence_steps SET subject = ?, template = ? WHERE id = ?`;
+                        const [results] = await connection.execute(query, [subject, template, step]);
+                        res.status(200).json({ data: results });
+                    } catch (error) {
+                        res.status(500).json({ error: 'Failed to get data BB' });
                     }
+                }
+            } else {
+                const c_id = req.query?.s_id ? req.query?.s_id : null;
+                const { stepType, intervalTime, taskPriority, taskNote, subject, template, status, execution_date } = req.body;
+                try {
+                    const query = 'INSERT INTO sequence_steps (sequence_id, step_type, interval_time, execution_date, priority, note, subject, template, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+                    const values = [c_id, stepType, intervalTime, execution_date, taskPriority, taskNote, subject, template, status];
+                    const [results] = await connection.execute(query, values);
                     res.status(200).json({ data: results });
-                });
-                connection.end();
+                } catch (error) {
+                    res.status(500).json({ error: 'Failed to get data BB' });
+                }
             }
-        } else {
-            res.status(500).json({ error: 'Contact not found!' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
-
-async function handleDeleteRequest(req, res) {
-    try {
-        const c_id = req.query?.c_id;
-        if (c_id) {
-            const connection = createConnection({ host: process.env.RDS_HOSTNAME, user: process.env.RDS_USERNAME, password: process.env.RDS_PASSWORD, database: process.env.RDS_DATABASE });
-            connection.connect((err) => {
-                if (err) {
-                    res.status(500).json({ error: 'Failed to connect to database' });
-                    return;
-                }
-            });
-            const query = `DELETE FROM contacts WHERE id = ${c_id}`;
-            connection.query(query, (err, results) => {
-                if (err) {
-                    res.status(500).json({ error: 'Failed to get data from database' });
-                    return;
-                }
-                res.status(200).json({ data: results });
-            });
-            connection.end();
-        } else {
-            res.status(500).json({ error: 'Contact not found!' });
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to get data BB' });
+        } finally {
+            await connection.end();
         }
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
--- src/pages/api/v1/webhooks/sendEmail.js
+++ src/pages/api/v1/webhooks/sendEmail.js
@@ -1,15 +1,8 @@
 const mysql = require('mysql2/promise');
-const nodemailer = require("nodemailer");
-
-const BASE_URL = 'http://localhost:3000';
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        if (req.query?.test) {
-            sendTestEmail(req, res);
-        } else {
-            handlePostRequest(req, res);
-        }
+        handlePostRequest(req, res);
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
@@ -24,30 +17,6 @@ const createConnection = async () => {
         database: process.env.RDS_DATABASE
     });
 };
-
-async function sendTestEmail(req, res) {
-    try {
-        const { subject, template } = req.body;
-        let html = template;
-        html = html.replace('{message}', req.body.message)
-        const mailOptions = {
-            from: 'manish.mailbox94@gmail.com',
-            to: 'devops.mailbox1@gmail.com',
-            subject: subject,
-            html: html
-        };
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                res.status(200).json({ message: "Failed to send email" });
-            } else {
-                res.status(200).json({ name: "Email sent successfully!" });
-            }
-        });
-
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
 
 async function handlePostRequest(req, res) {
     try {
@@ -73,31 +42,14 @@ async function handlePostRequest(req, res) {
                             senderName = val.value
                         }
                     });
-
                     const [prospects] = await connection.execute(`SELECT * FROM email_prospects WHERE email_id = ${emailData[0]['id']} AND email_sent = '2'`);
-                    const transporter = nodemailer.createTransport(smtpData);
                     if (prospects) {
                         for (let i = 0; i < prospects.length; i++) {
-                            let template = emailData[0]['template'];
-                            const receiver_data = JSON.parse(prospects[i]['receiver_data']);
-                            const trackingUrl = `${BASE_URL}/api/v1/trackemail?state=${prospects[i]['id']}`
-                            template = template.replace(/{{firstName}}/g, receiver_data['firstName'])
-                            template = template.replace(/{{senderName}}/g, senderName);
-                            template = `<html><style> .tracking-pixel { display: block; width: 1px; height: 1px; overflow: hidden;}</style>
-                            <body><img src="${trackingUrl}" style="display:none;" />
-                            ${template} </body></html>`
-                            const mailOptions = {
-                                from: emailData[0]['mail_from'],
-                                to: receiver_data['label'],
-                                subject: emailData[0]['subject'],
-                                html: template
-                            };
-                            let upquery = `UPDATE email_prospects SET message = ?, email_sent = ? WHERE id = ?`;
                             try {
-                                await transporter.sendMail(mailOptions);
+                                await sendEMail(smtpData, emailData[0], prospects[i], senderName);
+                                let upquery = `UPDATE email_prospects SET message = ?, email_sent = ? WHERE id = ?`;
                                 await connection.execute(upquery, ['Email sent successfully', '1', prospects[i]['id']]);
                             } catch (error) {
-                                console.log(error)
                                 await connection.execute(upquery, ['Failed to send email', '0', prospects[i]['id']]);
                             }
                         }
