3218b5f0c936de6e0c93e6f366bdb5554b19ee11
realcanadafirst <realcanadafirst@gmail.com>
2024-05-24 00:10:30 +0530

    Update uploads.js

--- src/pages/api/v1/uploads.js
+++ src/pages/api/v1/uploads.js
@@ -1,4 +1,4 @@
-import { createConnection } from 'mysql2';
+import { createConnection } from '@ft/lib/dbconnection';
 import formidable from 'formidable-serverless'
 import xlsx from 'xlsx';
 export const config = {
@@ -18,7 +18,7 @@ export default function handler(req, res) {
 async function handlePostRequest(req, res) {
     try {
         const form = new formidable();
-        form.parse(req, (err, fields, files) => {
+        form.parse(req, async (err, fields, files) => {
             if (err) {
                 res.status(500).json({ error: 'Error parsing form data' });
                 return;
@@ -31,39 +31,35 @@ async function handlePostRequest(req, res) {
                 const workbook = xlsx.readFile(uploadedFile.path);
                 const first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
                 const data = xlsx.utils.sheet_to_json(first_worksheet, { header: 1 });
-                const connection = createConnection({
-                    host: process.env.RDS_HOSTNAME,
-                    user: process.env.RDS_USERNAME,
-                    password: process.env.RDS_PASSWORD,
-                    database: process.env.RDS_DATABASE,
-                });
-                connection.connect((err) => {if (err) {res.status(500).json({ error: 'Failed to connect to database' });return;}});
+                const connection = await createConnection();
+                connection.connect((err) => { if (err) { res.status(500).json({ error: 'Failed to connect to database' }); return; } });
                 let failed = 0;
                 let totalrecords = 0;
-                data.forEach((element, index) => {
-                    if (index > 0 && (element[0] !== undefined || element[1] !== undefined || element[2] !== undefined || element[3] !== undefined)) {
-                        totalrecords++;
-                        try {
-                            const query = 'INSERT INTO Contacts (firstName, lastName, email, phoneNumber, organization_name) VALUES (?, ?, ?, ?, ?)';
-                            connection.query(query, [element[0], element[1], element[2], element[3], element[4]], (err, results) => {
-                                if (err) {
-                                    failed++;
-                                    if (err.code === 'ER_DUP_ENTRY' && err?.sqlMessage) {
-                                        const query = 'INSERT INTO error_log (message) VALUES (?)';
-                                        connection.query(query, [err.sqlMessage], (err, results) => {});
-                                    }
-                                }
-                            });
-                        } catch (error) {
-                            failed++;
+                try {
+                    for (let index = 0; index < data.length; index++) {
+                        const element = data[index];
+                        if (index > 0 && (element[0] !== undefined || element[1] !== undefined || element[2] !== undefined || element[3] !== undefined)) {
+                            totalrecords++;
+                            try {
+                                const query = 'INSERT INTO Contacts (firstName, lastName, email, phoneNumber, organization_name) VALUES (?, ?, ?, ?, ?)';
+                                await connection.execute(query, [element[0], element[1], element[2], element[3], element[4]]);
+                            } catch (error) {
+                                const query = 'INSERT INTO error_log (message) VALUES (?)';
+                                await connection.execute(query, [error.sqlMessage]);
+                                failed++;
+                            }
                         }
                     }
-                });
-                res.status(200).json({ data: totalrecords, message: `Out of ${totalrecords} Records ${totalrecords - failed} Records uploaded successfully` });
-                return;
+                    res.status(200).json({ data: totalrecords, message: `Out of ${totalrecords} Records ${totalrecords - failed} Records uploaded successfully` });
+                } catch (error) {
+                    console.log(error)
+
+                    res.status(500).json({ error: 'Failed to insert data in database.' });
+                } finally {
+                    await connection.end();
+                }
             } else {
                 res.status(400).json({ error: 'Invalid File found in request' });
-                return;
             }
         });
     } catch (error) {
