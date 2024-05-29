db279fbd1e0ca94ccefa42a5690965f1fdbc5a76
realcanadafirst <realcanadafirst@gmail.com>
2024-05-30 01:10:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     unoptimized: true,
     domains: ['d2d22nphq0yz8t.cloudfront.net', 'cdn.tailgrids.com']
