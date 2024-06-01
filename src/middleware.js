046d4a46445025d36deb1f1fd76b80832f042f5b
realcanadafirst <realcanadafirst@gmail.com>
2024-06-01 16:52:40 +0530

    access token issue

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,21 @@
 import { NextResponse } from 'next/server';
+
 export async function middleware(req) {
     const { pathname } = req.nextUrl;
     const response = NextResponse.next();
     if (pathname.startsWith('/api/v1')) {
+        console.log(req.headers)
         const access_token = req.headers.get('access_token');
         const user_hash = req.headers.get('user_hash');
         if (access_token && user_hash) {
             const valid_token = await fetch(`${req.nextUrl.origin}/api/auth/verify`, { headers: { access_token, user_hash } });
             if (!valid_token.ok) {
-                return new NextResponse('Unauthorized', { status: 401 });
+                return new NextResponse(access_token+'__'+user_hash, { status: 401 });
             } else {
                 return response;
             }
         } else {
-            return new NextResponse('Unauthorized', { status: 401 });
+            return new NextResponse(access_token+'__'+user_hash, { status: 401 });
         }
     }
     return response;
